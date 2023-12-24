5c0128943c64fef018d446d6a56763fa87482f6c
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-12-24 14:27:04 -0700

    created get categories view function

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import CategorySerializer
+from .models import Category
 
-# Create your views here.
+@api_view(['GET'])
+def categories_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
