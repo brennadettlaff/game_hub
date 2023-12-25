b7a3be1c2da4064f272b140b18ece8eb7b268f97
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-12-24 20:35:22 -0700

    added functionality to randomize and select 10

--- frontend/src/Components/CreateList.jsx
+++ frontend/src/Components/CreateList.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const CreateList = () => {
     const [categories, setCategories] = useState([])
+    const [randomList, setRandomList] = useState([])
 
     useEffect(() => {
         getCategories();
@@ -11,8 +12,29 @@ const CreateList = () => {
     async function getCategories(){
         let response = await axios.get('http://127.0.0.1:8000/api/categories/')
         setCategories(response.data)
-        console.log("Test", response.data)
+        getRandom(categories)
     }
+
+    function getRandom(list) {
+        const shuffledList = [...list];
+        for (let i = shuffledList.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+        }
+        setRandomList(shuffledList.slice(0, 10));
+      }
+      
+      return (
+        <div>
+            <h2>Item List</h2>
+            <ul>
+                {randomList.map(item => (
+                <li key={item.id}>{item.category}</li>
+                // Adjust 'item.id' and 'item.name' based on your API response structure
+                ))}
+            </ul>
+        </div>
+      );
 
 }
 
